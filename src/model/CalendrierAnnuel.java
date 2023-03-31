cc782067ecd8ebe598336333effca83670da8732
Thaoumani Dini ali <dnt4068a@ens.fsi>
2023-03-31 15:12:11 +0200

    ver 1.10

--- src/model/CalendrierAnnuel.java
+++ src/model/CalendrierAnnuel.java
@@ -8,13 +8,48 @@ public class CalendrierAnnuel {
 		this.annee = new Mois[12];
 		
 		/**/
-		
+		this.annee[0] = new Mois("janvier", 31);
+		this.annee[1] = new Mois("fevrier", 28);
+		this.annee[2] = new Mois("mars", 31);
+		this.annee[3] = new Mois("avril", 30);
+		this.annee[4] = new Mois("mair", 31);
+		this.annee[5] = new Mois("juin", 30);
+		this.annee[6] = new Mois("juillet", 31);
+		this.annee[7] = new Mois("aout", 31);
+		this.annee[8] = new Mois("septembre", 30);
+		this.annee[9] = new Mois("octobre", 31);
+		this.annee[10] = new Mois("novembre", 30);
+		this.annee[11] = new Mois("decembre", 31);
+		/**/
 	}
 
 
 
 
-
+	
+	public boolean estLibre(int jour, int mois) {
+		return annee[mois].estLibre(jour);	
+		
+	}
+	
+	
+	public boolean reserver(int jour, int mois) {
+		boolean msg;
+		
+		try {
+			
+			annee[mois].Reserver(jour);
+			msg = true;
+		}catch (Exception e) {
+			
+			// TODO: handle exception
+			e.printStackTrace();
+			msg = false;
+			
+		}finally {
+			return msg;
+		}
+	}
 
 
 	///////INNER CLASS\\\\\\\
